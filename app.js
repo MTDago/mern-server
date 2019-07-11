d6ca69bd1a552ce3d21552c3582808a8a7115ec7
NikChao <leodabrim@gmail.com>
2019-07-11 11:26:08 +1000

    Formatted app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+//Requires
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//App Setup
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
 
+//Database Connection goes here
+
+//Routes
 app.get('/', (req, res) => {
     res.send(JSON.stringify({ Hello: 'World' }));
 });
 
+//Start the server!
 app.listen(port, () => {
     console.log(`Server listening to localhost: ${port}`);
 });
