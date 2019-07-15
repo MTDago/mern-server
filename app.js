6a8abb115c5c8e02306ac77aed9a5a90304f5edf
NikChao <leodabrim@gmail.com>
2019-07-15 16:07:31 +1000

    Added some broken tests that need fixing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Requires
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,11 +8,14 @@ const morgan = require('morgan');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-require('dotenv').config();
+// Router Methods
+const bookRouter = require('./Routes/books');
+const blogRouter = require('./Routes/blogs');
+const mailinglistRouter = require('./Routes/mailingList');
 
 // App Setup
 const app = express();
-const port = process.env.PORT || 5000; //TO DO: Add PORT to .env
+const port = process.env.PORT || 5000;
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
@@ -29,9 +33,9 @@ mongoose.connect(process.env.DB_PATH, { useNewUrlParser: true }, err => {
 app.get('/', (req, res) => {
     res.send(JSON.stringify({ Hello: 'World' }));
 });
-app.use('/mailinglist', require('./Routes/mailingList'));
-app.use('/books', require('./Routes/books'));
-app.use('/blogs', require('./Routes/blogs'));
+app.use('/mailinglist', mailinglistRouter);
+app.use('/books', bookRouter);
+app.use('/blogs', blogRouter);
 
 // Start the server!
 app.listen(port, () => {
