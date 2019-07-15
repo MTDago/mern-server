0fb043cfda7bdbbf442b4f251cce4a8f7f802fcf
Michael Twitt <michael.twitt@hotmail.com>
2019-07-16 08:59:23 +1000

    Authentication setup, add 'secure' to any route that needs to be secured

---
+++ Routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const User = require('../models/User')
+const passport = require('passport');
+const router = express.Router();
+const {
+    login
+} = require('../middleware/auth')
+
+router.post('/login', login, (req, res) => {
+    res.json(req.user.email)
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.sendStatus(200)
+});
+
+module.exports = router;
