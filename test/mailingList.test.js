dd01771d9a664388b953d034df27a55087698791
NikChao <leodabrim@gmail.com>
2019-07-23 09:48:15 +1000

    Testing now works for blogs (get, post and get :id) and mailing list (get and post), books tests work if you remove secure from the routes in app.js

---
+++ test/mailingList.test.js
@@ -0,0 +1,49 @@
+process.env.NODE_ENV = 'test';
+
+let mongoose = require('mongoose');
+let Blog = require('../Models/blog');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Mailing List', () => {
+    // after(done => {
+    //     Blog.deleteOne({}, err => {
+    //         done();
+    //     });
+    // });
+
+    describe('/POST MailingList', () => {
+        it('it should POST an email to the mailing list', done => {
+            let info = {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'example@test.com'
+            };
+            chai.request('http://localhost:5000')
+                .post('/mailinglist')
+                .send(info)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+
+    describe('/get MailingList', () => {
+        it('it should get the mailing list from the API', done => {
+            chai.request('http://localhost:5000')
+                .get('/mailinglist')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+                });
+        });
+    });
+});
