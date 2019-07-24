41b594343b2bad007447610f024bac99243c43b3
NikChao <leodabrim@gmail.com>
2019-07-25 09:31:14 +1000

    Stripe fixing

--- Routes/payments.js
+++ Routes/payments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const stripeChargeCallback = res => (stripeErr, stripeRes) => {
+const stripeChargeCallback = (stripeErr, stripeRes) => {
     if (stripeErr) {
         res.status(500).send({ error: stripeErr });
     } else {
@@ -18,12 +18,13 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    console.log(process.env.STRIPE_SECRET_KEY);
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
         currency: 'aud'
     };
-    stripe.charges.create(body, stripeChargeCallback(res));
+    stripe.charges.create(body, stripeChargeCallback(err, res));
 });
 
 module.exports = router;
