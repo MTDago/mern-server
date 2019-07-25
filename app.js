5b15f972f07c8f1861b4594deb4a38f477092019
NikChao <leodabrim@gmail.com>
2019-07-25 10:08:29 +1000

    CORS revert

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ const port = process.env.PORT || 5000; //TO DO: Add PORT to .env
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-// app.options('*', cors());
-// app.use('*', cors({ credentials: true, origin: true }));
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+app.options('*', cors());
+app.use('*', cors({ credentials: true, origin: true }));
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next();
+// });
 
 app.use(cookieParser());
 app.use(
